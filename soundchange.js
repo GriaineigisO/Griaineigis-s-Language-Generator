cdd2b7ee18e8b39eee20bc3096f3dfededcd64cf
Cermait <markbodonnell@gmail.com>
2024-05-17 02:35:04 +0100

    Update soundchange.js

--- soundchange.js
+++ soundchange.js
@@ -1296,7 +1296,7 @@ function createAccNomSg() {
 //takes a noun and puts it in the genitive singular
 function createGenNomSg() {
     let sgSuffix = document.getElementById("singular-suffix").value;
-    let genPrefix = document.getElementById("accusative-prefix").value;
+    let genPrefix = document.getElementById("genetive-prefix").value;
     let spanNoun = document.getElementsByClassName("noun-gen-sg")
     for (i = 0; i < spanNoun.length; i++) {
         let nounGenSg = genPrefix + spanNoun[i].innerHTML + sgSuffix;
@@ -1342,7 +1342,7 @@ function createNounAccPl() {
 //takes a noun and puts it in the genitive plural
 function createNounGenPl() {
     let plSuffix = document.getElementById("plural-suffix").value;
-    let genPrefix = document.getElementById("accusative-prefix").value;
+    let genPrefix = document.getElementById("genitive-prefix").value;
     let spanNoun = document.getElementsByClassName("noun-gen-pl")
     for (i = 0; i < spanNoun.length; i++) {
         let nounGenPl = genPrefix + spanNoun[i].innerHTML + plSuffix;
